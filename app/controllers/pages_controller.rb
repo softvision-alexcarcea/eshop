2fbe9a69859c672e7b4d38eef69c494574ae6fe1
Alex Carcea <alexandru.carcea@softvision.ro>
2012-11-07 12:25:04 +0200

    Removed pages controller

--- app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-  def index
-    @title = "Home Page"
-    @root = true
-    @products = Product.page(params[:page]).per(@products_per_page)
-  end
-end

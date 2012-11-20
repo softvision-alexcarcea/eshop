a35253ac59f1250fc1e9736fcf1a62e605ca22de
Alex Carcea <alexandru.carcea@softvision.ro>
2012-11-20 18:47:25 +0200

    Tags and collections eye-candy w/ Select2

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
     source = params[:query].blank? ?
       ActsAsTaggableOn::Tag :
       ActsAsTaggableOn::Tag.named_like(params[:query])
-    @tags = source.page(params[:page]).per(params[:per] || 5)
+    @tags = source.page(params[:page]).per(params[:per])
     
     respond_to do |format|
       format.json { render json: @tags.to_json }
